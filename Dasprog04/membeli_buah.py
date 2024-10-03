4a8fe56bd284aa1d53534ae78561b14adbfe201c
Kayla Riza <kaylarizaa@gmail.com>
2024-10-03 21:16:57 +0700

    Update membeli_buah.py

--- Dasprog04/membeli_buah.py
+++ Dasprog04/membeli_buah.py
@@ -1,14 +1,9 @@
 n, k = input().split()
 a = list(map(int, input().split()))
-# counter = len([x for x in a if x <= int(k)])
-# if counter == 0:
-#     print("Minggir lu miskin.")
-# else:
-#     print(counter)
-buah = 0
-for jumlah in a:
-    if int(a) <= k:
-        break
-    else:
-        buah += 1
-print(buah)
+
+count = 0
+for i in a:
+    if i <= int(k):
+        count += 1
+
+print(count)
