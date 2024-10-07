ca042b745d884e00ca5d26472b50fcf15dd80d4a
Kayla Riza <kaylarizaa@gmail.com>
2024-10-07 23:52:34 +0700

    Update hutanpunyamasab.py
    AC harusnya

--- Dasprog04/hutanpunyamasab.py
+++ Dasprog04/hutanpunyamasab.py
@@ -8,39 +8,43 @@ gerakansalah = False
 
 def is_valid(row, col):
     return 0 <= row <= r and 0 <= col <= c
-for _ in range(n+1):
-    for move in moves:
-        if move == 'L':
-            if is_valid(pos_i, pos_j - 1):
-                pos_j -= 1
-                jum_gold += gold[pos_i][pos_j] - 2
-            else:
-                gerakansalah = True
-                break
-        elif move == 'R':
-            if is_valid(pos_i, pos_j + 1):
-                pos_j += 1
-                jum_gold += gold[pos_i][pos_j] + 3
-            else:
-                gerakansalah = True
-                break
-        elif move == 'U':
-            if is_valid(pos_i - 1, pos_j):
-                pos_i -= 1
-                jum_gold += gold[pos_i][pos_j] + 3
-            else:
-                gerakansalah = True
-                break
-        elif move == 'D':
-            if is_valid(pos_i + 1, pos_j):
-                pos_i += 1
-                jum_gold += gold[pos_i][pos_j] - 2
-            else:
-                gerakansalah = True
-                break
+
+for move in moves:
+    if move == 'L':
+        if is_valid(pos_i, pos_j - 1):
+            pos_j -= 1
+            jum_gold += gold[pos_i][pos_j] - 2
+            gold[pos_i][pos_j]=0
         else:
             gerakansalah = True
             break
+    elif move == 'R':
+        if is_valid(pos_i, pos_j + 1):
+            pos_j += 1
+            jum_gold += gold[pos_i][pos_j] + 3
+            gold[pos_i][pos_j]=0
+        else:
+            gerakansalah = True
+            break
+    elif move == 'U':
+        if is_valid(pos_i - 1, pos_j):
+            pos_i -= 1
+            jum_gold += gold[pos_i][pos_j] + 3
+            gold[pos_i][pos_j]=0
+        else:
+            gerakansalah = True
+            break
+    elif move == 'D':
+        if is_valid(pos_i + 1, pos_j):
+            pos_i += 1
+            jum_gold += gold[pos_i][pos_j] - 2
+            gold[pos_i][pos_j]=0
+        else:
+            gerakansalah = True
+            break
+    else:
+        gerakansalah = True
+        break
 
 print(jum_gold)
 
