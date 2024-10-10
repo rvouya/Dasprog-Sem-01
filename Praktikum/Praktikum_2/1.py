53255f3f30c2a3c7fbcf1216951db3ef5cb4941e
Kayla Riza <kaylarizaa@gmail.com>
2024-10-10 17:45:27 +0700

    Update 1.py

--- Praktikum/Praktikum_2/1.py
+++ Praktikum/Praktikum_2/1.py
@@ -1,9 +1,1 @@
-t = int(input())
-for _ in range(t):
-    n, k = map(int, input().split())
-    a = list(map(int, input().split()))
-    q = int(input())
-    if q == 1:
-        print(sum(a))
-    elif q == 2:
-        print(min(a))
+#Yang buatt soal shareloc tak parani
